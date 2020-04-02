ca246585eaf209965d15fc3cdc52ec80299101c9
wberdowski <10144015+wberdowski@users.noreply.github.com>
2020-04-02 19:50:58 +0200

    Reorganized sample code.

---
+++ MineX-QuerySample/Source/Program.cs
@@ -0,0 +1,50 @@
+﻿using MineX.Query;
+using System;
+using System.Net.Sockets;
+
+namespace MineX.QuerySample
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+                // Create a new instance of QueryClient and set server endpoint to localhost:25565
+                var queryClient = new QueryClient("localhost", 25565);
+
+                // Send query server for basic statistics
+                var basicStats = queryClient.QueryBasicStats();
+
+                // Print basic statistics to console
+                Console.WriteLine("==  BASIC STATS ==");
+                foreach (var property in typeof(BasicStats).GetProperties())
+                {
+                    Console.WriteLine($"{property.Name.PadRight(12, ' ')} {property.GetValue(basicStats)}");
+                }
+
+                // Send query server for full statistics
+                var fullStats = queryClient.QueryFullStats();
+
+                // Print full statistics to console
+                Console.WriteLine("==  FULL STATS ==");
+                foreach (var property in typeof(FullStats).GetProperties())
+                {
+                    Console.WriteLine($"{property.Name.PadRight(12, ' ')} {property.GetValue(fullStats)}");
+                }
+            }
+            catch (TimeoutException)
+            {
+                Console.WriteLine("Query timed out.");
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("A connection with the Query server could not be established.");
+            }
+
+            // Keep console open until user presses any key
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MineX-RconSample/Source/Program.cs
@@ -0,0 +1,42 @@
+﻿using MineX.Rcon;
+using System;
+using System.Net.Sockets;
+
+namespace MineX.RconSample
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+                // Create a new instance of RconClient
+                var rconClient = new RconClient();
+
+                // Connect to the server at localhost:25575
+                rconClient.Connect("localhost");
+
+                // Login with password
+                rconClient.Login("password");
+
+                // Send command and get its output
+                var cmdOutput = rconClient.SendCommand("list");
+
+                // Print command output
+                Console.WriteLine(cmdOutput);
+            }
+            catch (IncorrectPasswordException)
+            {
+                Console.WriteLine("Incorrect RCON password.");
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("A connection with the RCON server could not be established.");
+            }
+
+            // Keep console open until user presses any key
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
